095cb427028df4cf6968ca60bc1a4595bfe575f0
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-06 23:38:21 +0300

    components react-4

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ import registerServiceWorker from './registerServiceWorker';
 
 function Hello(){
 	let phrase = "World"
-	return(
+	return (
 		<h1>Hello {4+8}!</h1>
 	)
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+function SayFullName(props){
+	return (
+		//if not use <div> we have browser mistake
+		<div>
+			<h1>name - {props.name}, surname- {props.surname}</h1>
+			<a href={props.link}> My profile</a>
+		</div>
+	)
+}
+
+ReactDOM.render(<SayFullName name="Antony" surname="Antony1" link="gmail.com" />, document.getElementById('root'));
 registerServiceWorker();
