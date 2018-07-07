24d7054d5aa1e6abff1023bf9e24112de82df56e
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-08 00:37:51 +0300

    create H,M,M components with imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid 'react-bootstrap/lib/Grid'; //object with class Container, if use {}, all components in lib, in our case -only part
-import Grid 'react-bootstrap/lib/Row';
-import Grid 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid'; //object with class Container, if use {}, all components in lib, in our case -only part
+
 import styled from 'styled-components';
 
 
@@ -12,23 +11,10 @@ import styled from 'styled-components';
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Antony" surname="Antony1" link="gmail.com" />
-        <SayFullName name="Ron" surname="Ron1" link="mail.com" />
-      </div>
+      
     );
   }
 }
 
-function SayFullName(props){
-  return (
-    //if not use <div> we have browser mistake
-    //props Object with attributes, props only for reading
-    <div>
-      <h1>name - {props.name}, surname- {props.surname}</h1>
-      <a href={props.link}> My profile</a>
-    </div>
-  )
-}
 
 export default App;
