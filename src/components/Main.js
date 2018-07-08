9b8d50fa42d0c25e604a6e75d85e1b190ffc8149
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-08 11:17:58 +0300

    add main backround and text-span

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,31 @@ import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
 
+const Repair = styled.div`
+	color:#fff;
+	font-weight:700;
+	line-height:50px;
+	font-size:40px;
+
+	span{
+		display:block;
+		font-size:24px;
+		line-height:30px;
+	}
+`
+
 class Main extends React.Component{
 	render(){
 		return(
-			<h1>Main</h1>
+			<Row>
+				<Col lg={5}>
+					<Repair>Качественный ремонт
+					<span>iphone за 35 минут и гарантия 1 год</span>
+					</Repair>	
+				</Col>
+				<Col lg={6} lgOffset={1}>	
+				</Col>
+			</Row>
 		)
 	}
 }
