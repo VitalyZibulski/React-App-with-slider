cf60b08730ae1ecb9108bdf39da36f3a2e99cb19
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-08 11:27:22 +0300

    change main

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,6 +18,28 @@ const Repair = styled.div`
 	}
 `
 
+const LightText = styled.div`
+	color: #ffffff;
+	font-size: 18px;
+	font-weight: 300;
+	line-height: 28px;
+	margin-top:54px;
+`
+
+const CallBtn = styled.button`
+	width: 247px;
+	height: 67px;
+	margin-top:32px;
+	background-color: #ffa14b;
+	border-radius:30px;
+	color: #ffffff;
+	font-size: 18px;
+	font-weight: bold;
+	line-height: 24px;
+`
+
+
+
 class Main extends React.Component{
 	render(){
 		return(
@@ -25,9 +47,16 @@ class Main extends React.Component{
 				<Col lg={5}>
 					<Repair>Качественный ремонт
 					<span>iphone за 35 минут и гарантия 1 год</span>
-					</Repair>	
+					</Repair>
+					<LightText>Оставьте заявку на бесплатную диагностику без очереди,
+					и получите защитное стекло, стоимостью 1000 рублей,
+					с установкой в подарок!
+					</LightText>
+					<CallBtn>Отправить заявку!
+					</CallBtn>
 				</Col>
-				<Col lg={6} lgOffset={1}>	
+				<Col lg={6} lgOffset={1}>
+					
 				</Col>
 			</Row>
 		)
